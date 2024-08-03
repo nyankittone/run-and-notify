c0685f56b0df931a4ee6b3b88d91009fc4bc2d62
nyankittone <farrisluke8@gmail.com>
2024-08-03 16:36:09 -0400

    regressed version number ig

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 // Set any of these values to NULL to give then blank names.
 #define PROGRAM_NAME ("run-and-notify")
-#define PROGRAM_VERSION ("0.1")
+#define PROGRAM_VERSION ("0.1-pre0") // I will want to find a better way to control naming here
 #define PROGRAM_AUTHOR ("nyankittone <3")
 #define AUTHOR_GITHUB ("https://github.com/nyankittone")
 
